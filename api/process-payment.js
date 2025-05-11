21445af725984fa35cfb76af917c1338ff055650
Raco Alderon <racoalderon02@gmail.com>
2025-05-11 21:07:38 +1000

    api

--- api/process-payment.js
+++ api/process-payment.js
@@ -1,21 +1,20 @@
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    // Payment information to display
-    const paymentInfo = {
+  // Edge function for listing payment instructions
+  const paymentInfo = [
+    {
       title: "Payment Instructions",
-      description: "To ensure smooth operation of the building, please make your payments on time.",
-      methods: [
-        {
-          type: "Bank Transfer",
-          details: "Account Number - 12345678, BSB - 987654"
-        }
-      ]
-    };
+      details: "To ensure smooth operation of the building, please make your payments on time."
+    },
+    {
+      title: "Bank Transfer",
+      details: "Account Number - 12345678, BSB - 987654"
+    },
+    {
+      title: "Online Payment Portal",
+      details: "Visit https://strata-estate.vercel.app/"
+    }
+  ];
 
-    // Respond with the payment information
-    return res.status(200).json(paymentInfo);
-  } else {
-    // Return a 405 Method Not Allowed for unsupported methods
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+  // Respond with the payment information
+  res.status(200).json(paymentInfo);
 }
